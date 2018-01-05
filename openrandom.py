91429c4b0f953f9c8b33ed0ee2d509a6e02706f0
shreelock <shreenath.dutt@gmail.com>
2018-01-05 18:04:48 -0500

    added openrandom

---
+++ openrandom.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+from random import randint
+
+def unix_find(pathin):
+    return [os.path.join(path, file)
+            for (path, dirs, files) in os.walk(pathin)
+            for file in files]
+
+pathlist = unix_find('.')[-1000:]
+print "Total files in path :", len(pathlist)
+corrlist =[]
+
+for path in pathlist:
+  if(path.find('.mkv') !=-1):
+    corrlist.append(path)
+
+print "Total mkv files :", len(corrlist)
+rint = randint(0,len(corrlist))
+rfile = corrlist[rint]
+print "Random selected file :", rfile
+command = "gnome-open \"{}\"".format(rfile)
+subprocess.call(command, shell=True)
